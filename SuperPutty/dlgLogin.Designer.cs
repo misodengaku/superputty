dca74f9fee2e77c9137ffe69b26c3b42e0fe6ea4
misodengaku <misodengaku@gmail.com>
2014-04-25 02:10:47 +0900

    dlgLogin.cs localized (ja-jp)

--- SuperPutty/dlgLogin.Designer.cs
+++ SuperPutty/dlgLogin.Designer.cs
@@ -28,93 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBoxUsername = new System.Windows.Forms.TextBox();
-            this.textBoxPasssword = new System.Windows.Forms.TextBox();
-            this.checkBoxRemember = new System.Windows.Forms.CheckBox();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Username";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 38);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Password";
-            // 
-            // button1
-            // 
-            this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(147, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Ok";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBoxUsername
-            // 
-            this.textBoxUsername.Location = new System.Drawing.Point(78, 6);
-            this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(144, 20);
-            this.textBoxUsername.TabIndex = 1;
-            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // textBoxPasssword
-            // 
-            this.textBoxPasssword.Location = new System.Drawing.Point(78, 35);
-            this.textBoxPasssword.Name = "textBoxPasssword";
-            this.textBoxPasssword.Size = new System.Drawing.Size(144, 20);
-            this.textBoxPasssword.TabIndex = 2;
-            this.textBoxPasssword.UseSystemPasswordChar = true;
-            this.textBoxPasssword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // checkBoxRemember
-            // 
-            this.checkBoxRemember.AutoSize = true;
-            this.checkBoxRemember.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxRemember.Location = new System.Drawing.Point(78, 61);
-            this.checkBoxRemember.Name = "checkBoxRemember";
-            this.checkBoxRemember.Size = new System.Drawing.Size(104, 19);
-            this.checkBoxRemember.TabIndex = 4;
-            this.checkBoxRemember.Text = "Remember me";
-            this.checkBoxRemember.UseVisualStyleBackColor = true;
-            this.checkBoxRemember.CheckedChanged += new System.EventHandler(this.checkBoxRemember_CheckedChanged);
-            // 
-            // dlgLogin
-            // 
-            this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 116);
-            this.Controls.Add(this.checkBoxRemember);
-            this.Controls.Add(this.textBoxPasssword);
-            this.Controls.Add(this.textBoxUsername);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "dlgLogin";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Login";
-            this.Shown += new System.EventHandler(this.dlgLogin_Shown);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dlgLogin));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBoxUsername = new System.Windows.Forms.TextBox();
+			this.textBoxPasssword = new System.Windows.Forms.TextBox();
+			this.checkBoxRemember = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.Name = "label1";
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.Name = "label2";
+			// 
+			// button1
+			// 
+			resources.ApplyResources(this.button1, "button1");
+			this.button1.Name = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBoxUsername
+			// 
+			resources.ApplyResources(this.textBoxUsername, "textBoxUsername");
+			this.textBoxUsername.Name = "textBoxUsername";
+			this.textBoxUsername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// textBoxPasssword
+			// 
+			resources.ApplyResources(this.textBoxPasssword, "textBoxPasssword");
+			this.textBoxPasssword.Name = "textBoxPasssword";
+			this.textBoxPasssword.UseSystemPasswordChar = true;
+			this.textBoxPasssword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// checkBoxRemember
+			// 
+			resources.ApplyResources(this.checkBoxRemember, "checkBoxRemember");
+			this.checkBoxRemember.Name = "checkBoxRemember";
+			this.checkBoxRemember.UseVisualStyleBackColor = true;
+			this.checkBoxRemember.CheckedChanged += new System.EventHandler(this.checkBoxRemember_CheckedChanged);
+			// 
+			// dlgLogin
+			// 
+			this.AcceptButton = this.button1;
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.checkBoxRemember);
+			this.Controls.Add(this.textBoxPasssword);
+			this.Controls.Add(this.textBoxUsername);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "dlgLogin";
+			this.Shown += new System.EventHandler(this.dlgLogin_Shown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
